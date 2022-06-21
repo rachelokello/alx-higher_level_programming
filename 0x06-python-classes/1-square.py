d9fa326c90df8cc90db445ef16c0ae9136c27d7f
rachelokello <okelloawuor@gmail.com>
2022-06-21 17:51:40 +0300

    objecct and classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""Square Class
+A Square Class
+"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
 
     def __init__(self, size):
-        """Initializes the data."""
-        self.__size = size
+        """__init__
+        The __init__ method initializes the size value
+        of the square.
+        Attributes:
+            size (int): The size of the square.
+        """
+        self.__size = 
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+"""Square Class
+A Square Class
+"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    """
 
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """__init__
+        The __init__ method initializes the size value of the square.
+        Attributes:
+            size (:obj:`int`, optional): The size of the square.
+        Raises:
+            TypeError: If `size` type is not `int`.
+            ValueError: If `size` is less than `0`.
+        """
+
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
         self.__size = size
