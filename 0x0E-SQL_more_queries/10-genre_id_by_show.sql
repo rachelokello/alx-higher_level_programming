266f9b3f29d652d09d2a8a025e1deeca61499b51
rachelokello <okelloawuor@gmail.com>
2022-08-10 18:46:03 +0300

    correction

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,4 @@
--- Prints and joins records from two tables with a matching field
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
-    FROM tv_genres
-    LEFT JOIN tv_show_genres
-        ON tv_genres.id = tv_show_genres.genre_id
-    RIGHT JOIN tv_show_ratings
-        ON tv_show_ratings.show_id = tv_show_genres.show_id
-    WHERE tv_genres.name IS NOT NULL
-    GROUP BY tv_genres.id
-    ORDER BY rating DESC;
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all rows of a database that have one column in common
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
